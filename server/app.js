847b0a5c1c4b1d9dee8e9d3ddc8cd792aff0acd7
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-10-14 18:54:41 +0200

    favicon fixed

--- server/app.js
+++ server/app.js
@@ -61,7 +61,7 @@ app.use(require('node-sass-middleware')({
       
 
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 
 hbs.registerHelper('ifUndefined', (value, options) => {
